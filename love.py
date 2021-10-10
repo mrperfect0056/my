96374352e2b75a63ff2dbed1db66e657c3dcb911
Mr.Devil <87979690+mrperfect0056@users.noreply.github.com>
2021-10-11 00:37:02 +0600

    Add files via upload

---
+++ love.py
@@ -0,0 +1,126 @@
+#Love Calculator
+
+import os
+os.system('clear')
+col = '\033[1;95m'
+
+logo = '''
+
+\033[1;91m=======================================
+\033[1;96mAuthor  \033[1;93m: \033[1;92mLove
+\033[1;96mYouTube \033[1;93m: \033[1;92mhttps://youtu.be/nSGu4ZfinqE
+\033[1;96mGitHub  \033[1;93m: \033[1;92mhttps://github.com/mrperfect0056
+\033[1;96mBlogger \033[1;93m: \033[1;92mhttps://www.facebook.com/darkhacker
+\033[1;91m======================================="""
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print logo
+		print("\r\033[1;96m \x1b[1;93mDark hacker \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
+back = 0
+ 
+Just For Fun
+'''
+print col+logo
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+ 
+ 
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; Android 8.1.0; Chrome/79.0.3945.116) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.116 Mobile Safari/537.36')]
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+ 
+ 
+def keluar():
+	print "\033[1;96m[!] \x1b[1;91mExit"
+	os.sys.exit()
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+##### LOGO #####
+logo = """
+000000000000000000000000000000000000
+000000000000000000000000000000000000
+pppppppppppppppppppppppppppppppppppp
+pppppppppppppppppppppppppppppppppppp
+
+user = '1'
+password = '1'
+
+while True :
+	us = raw_input('Enter Tools User: ')
+	if us==user:
+		print 'Correct Username'
+		passs = raw_input('Enter Tools Password: ')
+		if passs==password:
+			print'Correct Password\nWelcome To Love World'
+			break
+		else:
+			print 'Wrong Password'
+	else:
+		print ' Wrong Username'
+		os.system('xdg-open https://www.facebook.com/darkhacker07/')
+print'please wait...'
+import time,os
+time.sleep(2)
+os.system('clear')
+col = '\033[1;95m'
+logo = ''' 
+_
+(00000          00000)
+(000000      000000)
+(0000  00 00  0000)
+(0000    000    0000)
+(0000               0000)
+'''
+print col+logo
+import time,os,random
+FN = 'Liza' , 'Anisa' , 'Meda' , 'Lamia' , 'Fatema' , ' Habiba' , 'Sultana' , 'Runa' , 'Maisa' , 'Lota' , 'Nipu' , 'Rusa' , 'Rusu' , 'Nazma' , ' Mansura' , 'Nadi' , 'Nadia' , 'Rumi' , 'Farida' , 'Ruzi' , 'Somaiya' , 'Tamima' ,arisha' ,bristy' , kulsum' ,Mim' ,Metu' ,Mimia' , 'Marufa' , 'Tamanna' , 'Taiyeba'
+MN = 'Abir' , 'Samir' , 'Adil' , ' Mohsin' , 'Akash' , 'Rakib' , 'Mehebub' , 'Tanzil' , 'Hasan' , 'Omar Raj' , 'Sojol' , 'Sojon' , 'Rahat' , 'Momin' , 'Navid' , 'Abid' , 'Shihan' , 'Rony' , 'Sifat' ,ahad' ,imtiaz' ,Rafin' ,Rifan' ,Raja' ,mahmud' , 'Rifat'
+col = '\033[1;92m'
+logo = '''
+1.Male
+2.Female
+'''
+print col+logo
+t =input('Select Your Gender: ')
+
+if t==1:
+  print 'You are A Male'
+  raw_input('Enter Your Name: ')
+  time.sleep(5)
+  print 'Your Lover Name: '
+  print random.choice(FN)
+
+elif t==2:
+  print 'You Are A Fimale'
+  raw_input('Enter Your Name: ')
+  time.sleep(5)
+  print 'Your Lover Name: '
+  print random.choice(MN)
+  
+else:
+  print '\033[1;91mThanks Bro '
